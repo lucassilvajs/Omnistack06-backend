d60e105896cc700ad01984537bd208e3ce0b1960
lucassilvajs <lucasjesusssilva@gmail.com>
2020-01-24 14:47:54 -0300

    Correções no server

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 const server = require('http').Server(app)
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, { origins: '*:*'});
 
 io.on('connection', socket => {
     socket.on('connectRoom', box => {
@@ -17,9 +17,6 @@ io.on('connection', socket => {
 });
 
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use("/files", express.static(path.resolve(__dirname, '..', 'tmp')))
 mongoose.connect('mongodb+srv://semana:semana@cluster0-43ss0.mongodb.net/tindev?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
@@ -33,6 +30,10 @@ app.use((req, res, next) => {
     return next();
 })
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/files", express.static(path.resolve(__dirname, '..', 'tmp')))
+
 app.use(require('./routes'));
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
