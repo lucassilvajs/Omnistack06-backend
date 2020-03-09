32befcab2740dc1962ee6623d95268c68ec166d8
Lucas de Jesus <lucasjesusssilva@gmail.com>
2020-03-09 09:52:43 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,14 @@ const routes = express.Router();
 
 const BoxController = require('./controllers/BoxController');
 const FileController = require('./controllers/FileController');
+const LeadController = require('./controllers/LeadController');
 
 routes.get('/teste', (req, res) => {
     return res.json({'sucesso': false});
 });
 
 routes.post("/boxes", BoxController.store)
+routes.post("/landing", LeadController.store)
 routes.get("/boxes/:id", BoxController.show)
 routes.post("/boxes/:id/files", multer(multerConfig).single('file'), FileController.store)
 
